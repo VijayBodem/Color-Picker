f6b303a17f82ad9a04dd444cc88955c5f5fb743a
VijayBodem <128890613+VijayBodem@users.noreply.github.com>
2023-05-02 12:33:21 +0530

    Color Picker

---
+++ color.js
@@ -0,0 +1,26 @@
+let grey = document.getElementById("colorPickerContainer");
+let hexCodeContent = document.getElementById("selectedColorHexCode");
+
+function changeToGrey() {
+    grey.style.backgroundColor = "#e0e0e0";
+    hexCodeContent.textContent = "#e0e0e0"
+}
+let green = document.getElementById("colorPickerContainer");
+
+function changeToGreen() {
+    green.style.backgroundColor = "#6fcf97";
+    hexCodeContent.textContent = "#6fcf97"
+}
+let skyblue = document.getElementById("colorPickerContainer");
+
+function changeToSkyBlue() {
+    skyblue.style.backgroundColor = "#56ccf2";
+    hexCodeContent.textContent = "#56ccf2"
+}
+
+let purple = document.getElementById("colorPickerContainer");
+
+function changeToPurple() {
+    purple.style.backgroundColor = "#bb6bd9"
+    hexCodeContent.textContent = "#bb6bd9"
+}
